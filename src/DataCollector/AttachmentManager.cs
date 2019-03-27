0b43d051a03e06e09e96d5f5cf3b4f4152c4d138
Abhishek Kumawat <abhishk@microsoft.com>
2019-03-27 10:30:42 +0530

    Updating attachment manager and coverage manager

--- src/DataCollector/AttachmentManager.cs
+++ src/DataCollector/AttachmentManager.cs
@@ -5,17 +5,41 @@ namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector
 {
     using System;
     using System.ComponentModel;
-    using Coverlet.Core;
-    using Coverlet.Core.Reporters;
     using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
 
     internal class AttachmentManager : IDisposable
     {
         private readonly DataCollectionSink dataSink;
+        private readonly CoverletLogger logger;
+        private readonly CoverletEqtTrace eqtTrace;
+        private readonly DataCollectionContext dataCollectionContext;
+        private readonly FileHelper fileHelper;
+        private readonly string reportFileName;
+        private readonly string reportDirectory;
 
-        public AttachmentManager(DataCollectionSink dataSink)
+        public AttachmentManager(DataCollectionSink dataSink, DataCollectionContext dataCollectionContext, CoverletLogger logger, CoverletEqtTrace eqtTrace, string reportFileName)
+            : this(dataSink,
+                  dataCollectionContext,
+                  logger,
+                  eqtTrace,
+                  reportFileName,
+                  Guid.NewGuid().ToString(),
+                  new FileHelper())
         {
+        }
+
+        public AttachmentManager(DataCollectionSink dataSink, DataCollectionContext dataCollectionContext, CoverletLogger logger, CoverletEqtTrace eqtTrace, string reportFileName, string reportDirectoryName, FileHelper fileHelper)
+        {
+            // Store input vars
             this.dataSink = dataSink;
+            this.dataCollectionContext = dataCollectionContext;
+            this.logger = logger;
+            this.eqtTrace = eqtTrace;
+            this.reportFileName = reportFileName;
+            this.fileHelper = fileHelper;
+
+            // Report directory to store the coverage reports.
+            this.reportDirectory = this.fileHelper.Combine(this.fileHelper.GetTempPath(), reportDirectoryName);
 
             // Register events
             this.dataSink.SendFileCompleted += this.OnSendFileCompleted;
@@ -23,32 +47,72 @@ namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector
 
         public void Dispose()
         {
-            // TODO: should we do file cleanup also here? OnSendFileCompleted is already doing cleanup.
-            this.dataSink.SendFileCompleted -= this.OnSendFileCompleted;
+            if (this.dataSink != null)
+            {
+                this.dataSink.SendFileCompleted -= this.OnSendFileCompleted;
+            }
         }
 
-        public void CreateAndSendCoverageAttachments(CoverageResult coverageResult)
+        public void SendCoverageReport(string coverageReport)
         {
-            // TODO: Catch exception as we have IO operations.
-            string attachmentPath = this.CreateAttachment(coverageResult);
-            this.SendAttachment(attachmentPath);
-        }
+            // Save coverage report to file
+            var coverageReportPath = this.SaveCoverageReport(coverageReport);
+            if (string.IsNullOrWhiteSpace(coverageReportPath)) return;
 
-        private void SendAttachment(string attachmentPath)
-        {
-            throw new NotImplementedException();
-        }
-
-        private string CreateAttachment(CoverageResult coverageResult)
-        {
-            var reporter = new ReporterFactory("json").CreateReporter();
-            var serializedResult = reporter.Report(coverageResult);
-            throw new NotImplementedException();
+            // Send coverage attachment to test platform.
+            this.SendAttachment(coverageReportPath);
         }
 
         private void OnSendFileCompleted(object sender, AsyncCompletedEventArgs e)
         {
-            // TODO: file cleanup
+            this.eqtTrace.Verbose("{0}: OnSendFileCompleted received", CoverletConstants.DataCollectorName);
+            this.CleanupReportDirectory();
+        }
+
+        private string SaveCoverageReport(string report)
+        {
+            try
+            {
+                this.fileHelper.CreateDirectory(this.reportDirectory);
+                var filePath = this.fileHelper.Combine(this.reportDirectory, this.reportFileName);
+                this.fileHelper.WriteAllText(filePath, report);
+                this.eqtTrace.Info("{0}: Saved coverage report to path: {1}", CoverletConstants.DataCollectorName, filePath);
+
+                return filePath;
+            }
+            catch (Exception ex)
+            {
+                this.eqtTrace.Error("{0}: Failed to save coverage report to file: {1} in directory: {2} with exception {3}", CoverletConstants.DataCollectorName, this.reportFileName, this.reportDirectory, ex);
+                this.logger.LogError(new CoverletDataCollectorException(Resources.FailedToSaveCoverageReport, ex));
+            }
+
+            return default(string);
+        }
+
+        private void SendAttachment(string attachmentPath)
+        {
+            if (this.fileHelper.Exists(attachmentPath))
+            {
+                // Send coverage attachment to test platform.
+                this.eqtTrace.Verbose("{0}: Sending attachment to test platform", CoverletConstants.DataCollectorName);
+                this.dataSink.SendFileAsync(this.dataCollectionContext, attachmentPath, false);
+            }
+        }
+
+        private void CleanupReportDirectory()
+        {
+            try
+            {
+                if (this.fileHelper.Exists(this.reportDirectory))
+                {
+                    this.fileHelper.Delete(this.reportDirectory, true);
+                    this.eqtTrace.Verbose("{0}: Deleted report directory: {1}", CoverletConstants.DataCollectorName, this.reportDirectory);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.eqtTrace.Warning("{0}: Failed to delete report directory: {1} with exception {2}", CoverletConstants.DataCollectorName, this.reportDirectory, ex);
+            }
         }
     }
 }
--- src/DataCollector/CoverageManager.cs
+++ src/DataCollector/CoverageManager.cs
@@ -4,53 +4,98 @@
 namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector
 {
     using System;
-    using System.Threading;
-    using System.Threading.Tasks;
     using Coverlet.Core;
+    using Coverlet.Core.Reporters;
 
-    internal class CoverageManager : IDisposable
+    internal class CoverageManager
     {
         private readonly Coverage coverage;
-        private readonly ManualResetEvent instrumentationCompletionEvent = new ManualResetEvent(true);
+        private readonly CoverletLogger logger;
+        private readonly CoverletEqtTrace eqtTrace;
 
-        public CoverageManager(CoverletSettings settings)
+        public IReporter Reporter { get; }
+
+        public CoverageManager(CoverletSettings settings, CoverletLogger logger, CoverletEqtTrace eqtTrace)
+            : this (settings,
+                  logger,
+                  eqtTrace,
+                  new ReporterFactory(CoverletConstants.DefaultReportFormat).CreateReporter())
         {
-            this.coverage = new Coverage(settings.TestModules[0], settings.IncludeFilters, settings.IncludeDirectories, settings.ExcludeFilters, settings.ExcludeSourceFiles, settings.ExcludeAttributes, settings.MergeWith, settings.UseSourceLink);
         }
 
-        public void Dispose()
+        public CoverageManager(CoverletSettings settings, CoverletLogger logger, CoverletEqtTrace eqtTrace, IReporter reporter)
         {
-            // Dispose wait handles
-            this.instrumentationCompletionEvent.Dispose();
+            // Store input vars
+            this.logger = logger;
+            this.eqtTrace = eqtTrace;
+            this.Reporter = reporter;
+
+            // Coverage object
+            this.coverage = new Coverage(
+                settings.TestModule,
+                settings.IncludeFilters,
+                settings.IncludeDirectories,
+                settings.ExcludeFilters,
+                settings.ExcludeSourceFiles,
+                settings.ExcludeAttributes,
+                settings.MergeWith,
+                settings.UseSourceLink);
         }
 
-        public async void StartInstrumentationAsync()
-        {
-            await Task.Run(() => this.StartInstrumentation());
-        }
-
-        public void WaitForInstrumentationComplete()
-        {
-            this.instrumentationCompletionEvent.WaitOne();
-        }
-
-        public CoverageResult GetCoverageResult()
-        {
-            return this.coverage.GetCoverageResult();
-        }
-
-        private void StartInstrumentation()
+        public void StartInstrumentation()
         {
             try
             {
                 // Instrument modules
-                this.coverage.PrepareModules(); // TODO: Log error in case of exception and restore the original modules. Skip coverage collection or error out after throwing error.
+                this.coverage.PrepareModules();
             }
-            finally
+            catch (Exception ex)
             {
-                // Set instrument compeltion wait handle.
-                this.instrumentationCompletionEvent.Set();
+                this.eqtTrace.Error("{0}: Failed to instrument modules with exception {1}", CoverletConstants.DataCollectorName, ex);
+                this.logger.LogError(new CoverletDataCollectorException(Resources.InstrumentationException, ex));
             }
+        }
+
+        public string GetCoverageReport()
+        {
+            // Get coverage result
+            var coverageResult = this.GetCoverageResult();
+            if (coverageResult == null) return null;
+
+            // Get coverage report in default format
+            var coverageReport = this.GetCoverageReport(coverageResult);
+            return coverageReport;
+        }
+
+        private CoverageResult GetCoverageResult()
+        {
+            try
+            {
+                return this.coverage.GetCoverageResult();
+            }
+            catch (Exception ex)
+            {
+                this.eqtTrace.Error("{0}: Failed to get coverage result with exception {1}", CoverletConstants.DataCollectorName, ex);
+                this.logger.LogError(new CoverletDataCollectorException(Resources.CoverageResultException, ex));
+            }
+
+            return default(CoverageResult);
+        }
+
+        private string GetCoverageReport(CoverageResult coverageResult)
+        {
+            try
+            {
+                return this.Reporter.Report(coverageResult);
+            }
+            catch (Exception ex)
+
+            {
+                this.eqtTrace.Error("{0}: Failed to get {1} report with exception {2}", CoverletConstants.DataCollectorName, this.Reporter.Format, ex);
+                this.logger.LogError(new CoverletDataCollectorException(Resources.CoverageResultException, ex));
+            }
+
+            return default(string);
         }
     }
 }
