4cda58485d44f889bbb1285c059a6c29a6cfdb05
Abhishek Kumawat <abhishk@microsoft.com>
2019-03-27 10:30:15 +0530

    Adding coverlet settings and its parser

--- src/DataCollector/CoverletSettings.cs
+++ src/DataCollector/CoverletSettings.cs
@@ -1,83 +1,42 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Text;
+
 namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector
 {
-    using System;
-    using System.Xml;
-
     internal class CoverletSettings
     {
-        public CoverletSettings(XmlElement configurationElement)
+        public string TestModule { get; set; }
+
+        public string[] IncludeFilters { get; set; }
+
+        public string[] IncludeDirectories { get; set; }
+
+        public string[] ExcludeFilters { get; set; }
+
+        public string[] ExcludeSourceFiles { get; set; }
+
+        public string[] ExcludeAttributes { get; set; }
+
+        public string MergeWith { get; set; }
+
+        public bool UseSourceLink { get; set; }
+
+        public override string ToString()
         {
-            this.ParseCoverletSettings(configurationElement);
-        }
+            var builder = new StringBuilder();
 
-        public string[] IncludeFilters { get; internal set; }
+            builder.AppendFormat("TestModule: {0}", this.TestModule);
+            builder.AppendFormat("IncludeFilters: {0}", string.Join(",", this.IncludeFilters));
+            builder.AppendFormat("IncludeDirectories: {0}", string.Join(",", this.IncludeDirectories));
+            builder.AppendFormat("ExcludeFilters: {0}", string.Join(",", this.ExcludeFilters));
+            builder.AppendFormat("ExcludeSourceFiles: {0}", string.Join(",", this.ExcludeSourceFiles));
+            builder.AppendFormat("ExcludeAttributes: {0}", string.Join(",", this.ExcludeAttributes));
+            builder.AppendFormat("MergeWith: {0}", this.MergeWith);
+            builder.AppendFormat("UseSourceLink: {0}", this.UseSourceLink);
 
-        public string[] IncludeDirectories { get; internal set; }
-
-        public string[] ExcludeFilters { get; internal set; }
-
-        public string[] ExcludeSourceFiles { get; internal set; }
-
-        public string[] ExcludeAttributes { get; internal set; }
-
-        public string MergeWith { get; internal set; }
-
-        public bool UseSourceLink { get; internal set; }
-
-        public string[] TestModules { get; internal set; }
-
-        private void ParseCoverletSettings(XmlElement configurationElement)
-        {
-            this.ParseIncludeFilters();
-            this.ParseIncludeDirectories();
-            this.ParseExcludeFilters();
-            this.ParseExcludeFilters();
-            this.ParseExcludeSourceFiles();
-            this.ParseMergeWith();
-            this.ParseUseSourceLink();
-            this.ParseTestModules();
-
-            // TODO: If code is common for parsing, merge the logic.
-        }
-
-        private void ParseTestModules()
-        {
-            // TODO: In all these methods, do validations as user can put anything. In case of invalidations, decide whether to throw error OR ignore those attribute with a warning.
-            throw new NotImplementedException();
-        }
-
-        private void ParseUseSourceLink()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ParseMergeWith()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ParseExcludeSourceFiles()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ParseIncludeFilters()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ParseExcludeFilters()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ParseIncludeDirectories()
-        {
-            // TODO: Directories of test modules should also be included.
-            throw new NotImplementedException();
+            return builder.ToString();
         }
     }
 }
---
+++ src/DataCollector/CoverletSettingsParser.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml;
+
+    internal class CoverletSettingsParser
+    {
+        private readonly CoverletLogger logger;
+        private readonly CoverletEqtTrace eqtTrace;
+
+        public CoverletSettingsParser(CoverletLogger logger, CoverletEqtTrace eqtTrace)
+        {
+            this.logger = logger;
+            this.eqtTrace = eqtTrace;
+        }
+
+        public CoverletSettings Parse(XmlElement configurationElement, IEnumerable<string> testModules)
+        {
+            var coverletSettings = new CoverletSettings
+            {
+                TestModule = this.ParseTestModule(testModules)
+            };
+
+            if (configurationElement != null)
+            {
+                coverletSettings.IncludeFilters = this.ParseIncludeFilters(configurationElement);
+                coverletSettings.IncludeDirectories = this.ParseIncludeDirectories(configurationElement);
+                coverletSettings.ExcludeFilters = this.ParseExcludeFilters(configurationElement);
+                coverletSettings.ExcludeSourceFiles = this.ParseExcludeSourceFiles(configurationElement);
+                coverletSettings.ExcludeAttributes = this.ParseExcludeAttributes(configurationElement);
+                coverletSettings.MergeWith = this.ParseMergeWith(configurationElement);
+                coverletSettings.UseSourceLink = this.ParseUseSourceLink(configurationElement);
+            }
+
+            if (this.eqtTrace.IsVerboseEnabled)
+            {
+                this.eqtTrace.Verbose("{0} - Initializing coverlet process with settings: {1}", CoverletConstants.DataCollectorName, coverletSettings.ToString());
+            }
+
+            return coverletSettings;
+        }
+
+        private string ParseTestModule(IEnumerable<string> testModules)
+        {
+            // Validate if atleast one source present.
+            if (testModules == null || !testModules.Any())
+            {
+                this.eqtTrace.Error("{0}: No test modules found", CoverletConstants.DataCollectorName);
+                this.logger.LogError(new CoverletDataCollectorException(Resources.NoTestModulesFound));
+            }
+
+            // Note:
+            // 1) .NET core test run supports one testModule per run. Coverlet also supports one testModule per run. So, we are using first testSource only and ignoring others.
+            // 2) If and when .NET full is supported with coverlet OR .NET core starts supporting multiple testModules, revisit this code to use other testModules as well.
+            return testModules?.FirstOrDefault();
+        }
+
+        private string[] ParseIncludeFilters(XmlElement configurationElement)
+        {
+            var includeFiltersElement = configurationElement[CoverletConstants.IncludeFiltersElementName];
+            return includeFiltersElement?.InnerText?.Split(',');
+        }
+
+        private string[] ParseIncludeDirectories(XmlElement configurationElement)
+        {
+            var includeDirectoriesElement = configurationElement[CoverletConstants.IncludeDirectoriesElementName];
+            return includeDirectoriesElement?.InnerText?.Split(',');
+        }
+
+        private string[] ParseExcludeFilters(XmlElement configurationElement)
+        {
+            var excludeFiltersElement = configurationElement[CoverletConstants.ExcludeFiltersElementName];
+            return excludeFiltersElement?.InnerText?.Split(',');
+        }
+
+        private string[] ParseExcludeSourceFiles(XmlElement configurationElement)
+        {
+            var excludeSourceFilesElement = configurationElement[CoverletConstants.ExcludeSourceFilesElementName];
+            return excludeSourceFilesElement?.InnerText?.Split(',');
+        }
+
+        private string[] ParseExcludeAttributes(XmlElement configurationElement)
+        {
+            var excludeAttributesElement = configurationElement[CoverletConstants.ExcludeAttributesElementName];
+            return excludeAttributesElement?.InnerText?.Split(',');
+        }
+
+        private string ParseMergeWith(XmlElement configurationElement)
+        {
+            var mergeWithElement = configurationElement[CoverletConstants.MergeWithElementName];
+            return mergeWithElement?.InnerText;
+        }
+
+        private bool ParseUseSourceLink(XmlElement configurationElement)
+        {
+            var useSourceLinkElement = configurationElement[CoverletConstants.UseSourceLinkElementName];
+            bool.TryParse(useSourceLinkElement?.InnerText, out var useSourceLink);
+            return useSourceLink;
+        }
+    }
+}
