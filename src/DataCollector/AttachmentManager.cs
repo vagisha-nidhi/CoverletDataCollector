51737c64b155cb082c1eae74416b093c6eb7c8b3
Abhishek Kumawat <abhishk@microsoft.com>
2019-04-12 15:48:44 +0530

    Moving sendFileCompleted to AttachmentManager

--- src/DataCollector/AttachmentManager.cs
+++ src/DataCollector/AttachmentManager.cs
@@ -4,24 +4,27 @@
 namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector.DataCollector
 {
     using System;
+    using System.ComponentModel;
     using System.IO;
     using Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector.Resources;
     using Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector.Utilities;
     using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
 
-    internal class AttachmentManager
+    internal class AttachmentManager : IDisposable
     {
         private readonly DataCollectionSink dataSink;
         private readonly TestPlatformEqtTrace eqtTrace;
+        private readonly TestPlatformLogger logger;
         private readonly DataCollectionContext dataCollectionContext;
         private readonly FileHelper fileHelper;
         private readonly DirectoryHelper directoryHelper;
         private readonly string reportFileName;
         private readonly string reportDirectory;
 
-        public AttachmentManager(DataCollectionSink dataSink, DataCollectionContext dataCollectionContext, TestPlatformEqtTrace eqtTrace, string reportFileName)
+        public AttachmentManager(DataCollectionSink dataSink, DataCollectionContext dataCollectionContext, TestPlatformLogger logger, TestPlatformEqtTrace eqtTrace, string reportFileName)
             : this(dataSink,
                   dataCollectionContext,
+                  logger,
                   eqtTrace,
                   reportFileName,
                   Guid.NewGuid().ToString(),
@@ -30,11 +33,12 @@ namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector.DataCo
         {
         }
 
-        public AttachmentManager(DataCollectionSink dataSink, DataCollectionContext dataCollectionContext, TestPlatformEqtTrace eqtTrace, string reportFileName, string reportDirectoryName, FileHelper fileHelper, DirectoryHelper directoryHelper)
+        public AttachmentManager(DataCollectionSink dataSink, DataCollectionContext dataCollectionContext, TestPlatformLogger logger, TestPlatformEqtTrace eqtTrace, string reportFileName, string reportDirectoryName, FileHelper fileHelper, DirectoryHelper directoryHelper)
         {
             // Store input vars
             this.dataSink = dataSink;
             this.dataCollectionContext = dataCollectionContext;
+            this.logger = logger;
             this.eqtTrace = eqtTrace;
             this.reportFileName = reportFileName;
             this.fileHelper = fileHelper;
@@ -42,6 +46,9 @@ namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector.DataCo
 
             // Report directory to store the coverage reports.
             this.reportDirectory = Path.Combine(Path.GetTempPath(), reportDirectoryName);
+
+            // Register events
+            this.dataSink.SendFileCompleted += this.OnSendFileCompleted;
         }
 
         public void SendCoverageReport(string coverageReport)
@@ -53,8 +60,15 @@ namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector.DataCo
             this.SendAttachment(coverageReportPath);
         }
 
-        public void OnSendFileCompleted()
+        public void Dispose()
         {
+            // Unregister events
+            if (this.dataSink != null)
+            {
+                this.dataSink.SendFileCompleted -= this.OnSendFileCompleted;
+            }
+
+            // Cleanup report directory
             this.CleanupReportDirectory();
         }
 
@@ -73,6 +87,20 @@ namespace Microsoft.TestPlatform.Extensions.CoverletCoverageDataCollector.DataCo
             {
                 var errorMessage = string.Format(Resources.FailedToSaveCoverageReport, CoverletConstants.DataCollectorName, this.reportFileName, this.reportDirectory);
                 throw new CoverletDataCollectorException(errorMessage, ex);
+            }
+        }
+
+        public void OnSendFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                this.eqtTrace.Verbose("{0}: SendFileCompleted received", CoverletConstants.DataCollectorName);
+                this.CleanupReportDirectory();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogWarning(ex.ToString());
+                this.Dispose();
             }
         }
 
